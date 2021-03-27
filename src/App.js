db21c7d8fd0398e968ee7565609f79451ecd7fb8
Izabella Queiroz Cardoso <izabella.cardoso@hiplatform.com>
2021-03-27 15:13:38 -0300

    components,property and state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from './components/Header'
 
 function App() {
+    const [projects, serProjects] = useState(['Desenvolvimento app', 'Desenvolvimento web'])
+    
+    function handleAddProject() {
+        serProjects([...projects, `novo projeto ${Date.now()}`])
+
+        console.log(projects)
+    }
     return (
         <>
-            <Header/>
-            <Header/>
+            <Header title="Projects"/>
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type='button' onClick={handleAddProject} >Adicionar Projeto </button>
         </>
     )
 }
